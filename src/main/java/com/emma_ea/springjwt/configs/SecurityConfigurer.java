529a109ab956b1813315fff19117da210aceb63a
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-03-05 23:17:17 +0000

    impl securityFilterChain

--- src/main/java/com/emma_ea/springjwt/configs/SecurityConfigurer.java
+++ src/main/java/com/emma_ea/springjwt/configs/SecurityConfigurer.java
@@ -1,4 +1,39 @@
 package com.emma_ea.springjwt.configs;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfigurer {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeRequests((auth) -> auth.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withUsername("user")
+                .password("pass")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
 }
