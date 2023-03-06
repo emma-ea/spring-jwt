b3690e3f00088a11a627491213d326d8cfd14174
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-03-06 22:19:17 +0000

    expire token

--- src/main/java/com/emma_ea/springjwt/util/JwtUtil.java
+++ src/main/java/com/emma_ea/springjwt/util/JwtUtil.java
@@ -47,6 +47,7 @@ public class JwtUtil {
                 .setClaims(claims)
                 .setSubject(subject)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() * 1000 * 60 * 60 * 10))
                 .signWith(SignatureAlgorithm.ES256, SECRET_KEY).compact();
     }
 
