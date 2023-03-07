05eff75e0492ffb8794117dfe9c29d7c5cfbe350
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-03-07 00:04:58 +0000

    generic http response

---
+++ src/main/java/com/emma_ea/springjwt/model/HttpResponse.java
@@ -0,0 +1,40 @@
+package com.emma_ea.springjwt.model;
+
+public class HttpResponse<T> {
+
+    private String status;
+    private String message;
+    private T data;
+
+    public HttpResponse() {}
+
+    public HttpResponse(String status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
